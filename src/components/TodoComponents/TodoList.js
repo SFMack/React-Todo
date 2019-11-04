ef359f6896642dcdf3e8b79c9bdf4d7b40a90d96
SFMack <sfcamack@icloud.com>
2019-11-03 18:24:31 -0800

    basic structure setup

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,14 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+import TodoForm from './TodoForm';
+
+export default function TodoList(props) {
+	return (
+		<>
+			<h1>Don't Forget!</h1>
+			<TodoForm />
+
+			{console.log(props.state)}
+		</>
+	);
+}
